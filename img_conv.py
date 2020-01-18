ddba77278d1b902fb42fc179c326db443d625e01
Abdelgo <am.ouakil@gmail.com>
2020-01-18 17:06:37 +0100

    V1_jpg_to_png_converter

--- img_conv.py
+++ img_conv.py
@@ -4,37 +4,39 @@ import sys
 from PIL import Image
 from PIL import ImageFilter
 
+
 # the funtion is called with sys.arg[1] = ./raw_images and sys.arg[2]= ./newfilespng
 # grab 1st and second argument
 
-#raw_images_file = argv[1]
-#new_images_file = argv[2]
+converted_images_file = sys.argv[1]
+raw_images_file = sys.argv[2]
 
 # 1s get the current Tab
-path = os.getcwd()
+current_folder = os.getcwd()
 #print ("The current working directory is %s" % path)
 
 # grab the new folder name
-converted_images_file = 'test'
-raw_images_file = 'Images'
+#converted_images_file = 'test'
+#raw_images_file = 'Images'
 
-converted_images_file_PATH = path + '/' + converted_images_file
-raw_images_file_PATH = path + '/' + raw_images_file
+
+converted_images_file_PATH = current_folder + '/' + converted_images_file
+raw_images_file_PATH = current_folder + '/' + raw_images_file
 
 
 #print("absname with path= %s" % os.path.abspath(path))
 
 print("exists? = %s" % os.path.exists(converted_images_file_PATH))
 
-#print("list = %s" % os.listdir(path))
+#print("list = %s" % os.listdir(current_folder))
 
 # check if is new/exists, if not create a new
 
 if os.path.exists(converted_images_file_PATH):
-    print(f'file already exist see the list below :\n {os.listdir(path)} ')
+    print(f"file already exist see the list below : {os.listdir(current_folder)}")
 else:
     os.mkdir(converted_images_file)
-    print(f'file {converted_images_file} created ! see the list below : \n {os.listdir(path)}')
+    print(f'file {converted_images_file} created ! see the list below : {os.listdir(current_folder)}')
 
 
 
